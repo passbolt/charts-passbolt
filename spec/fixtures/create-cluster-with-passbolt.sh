5d8fb3385864943d61c6677b2d5736afa9e26f7b
Daniel Del Rio Figueira <daniel@passbolt.com>
2024-04-15 17:23:38 +0200

    : Added helm repositories

--- spec/fixtures/create-cluster-with-passbolt.sh
+++ spec/fixtures/create-cluster-with-passbolt.sh
@@ -91,6 +91,8 @@ function installNginxIngress {
 }
 
 function installPassboltChart {
+	"$HELM_BINARY" repo add bitnami https://charts.bitnami.com/bitnami
+	"$HELM_BINARY" repo add passbolt-library https://download.passbolt.com/charts/passbolt-library
 	"$HELM_BINARY" dependency build
 	"$HELM_BINARY" install passbolt . -f ingress-values.yaml -n default
 	"$KUBECTL_BINARY" rollout status deployment passbolt-depl-srv --timeout=120s -n default
