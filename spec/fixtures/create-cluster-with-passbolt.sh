3364cd84dc473af7fd6141c1a718ec056074e2a9
Daniel Del Rio Figueira <daniel@passbolt.com>
2024-04-15 17:08:42 +0200

    : fix mkcert binary variable

--- spec/fixtures/create-cluster-with-passbolt.sh
+++ spec/fixtures/create-cluster-with-passbolt.sh
@@ -47,6 +47,7 @@ function getMkcert {
 		curl -JLO "https://dl.filippo.io/mkcert/latest?for=linux/amd64"
 		chmod +x mkcert-v*-linux-amd64
 		mv mkcert-v*-linux-amd64 mkcert
+		MKCERT_BINARY="./mkcert"
 	fi
 }
 
