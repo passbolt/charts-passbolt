465d9d865a36fd6a5b6dac0693b88a2d628b4178
Daniel Del Rio Figueira <daniel@passbolt.com>
2024-04-16 11:14:35 +0200

    : Increase logs

--- spec/tests/create_password.sh
+++ spec/tests/create_password.sh
@@ -4,7 +4,7 @@ function createPassword {
 	name="$1"
 	secret="$2"
 
-	./passbolt configure --serverAddress "https://${PASSBOLT_FQDN}" --userPassword "$PASSPHRASE" --userPrivateKeyFile 'secret.asc' 2>&1 >/dev/null
+	./passbolt configure --serverAddress "https://${PASSBOLT_FQDN}" --userPassword "$PASSPHRASE" --userPrivateKeyFile 'secret.asc'
 	./passbolt create resource --name "$name" Resource --password "$secret" -j
 
 }
