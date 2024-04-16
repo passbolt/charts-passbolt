ec0a8c99ec14c9901a3f7d789b87c4853e5ef932
Daniel Del Rio Figueira <daniel@passbolt.com>
2024-04-16 18:36:55 +0200

    : sleep for debugging

--- spec/create_password_spec.sh
+++ spec/create_password_spec.sh
@@ -44,12 +44,13 @@ function registerPassboltUser {
 
 	createGPGKey
 
-	curl -s "https://${PASSBOLT_FQDN}/setup/complete/${user_uuid}" \
+	echo curl -s "https://${PASSBOLT_FQDN}/setup/complete/${user_uuid}" \
 		-H "authority: ${PASSBOLT_FQDN}" \
 		-H "accept: application/json" \
 		-H "content-type: application/json" \
 		--data-raw "{\"authenticationtoken\":{\"token\":\"${user_token}\"},\"gpgkey\":{\"armored_key\":\"$(awk '{printf "%s\\n", $0}' public.asc)\"}}" \
 		--compressed
+	echo waitign for user
 }
 
 function createPassword {
