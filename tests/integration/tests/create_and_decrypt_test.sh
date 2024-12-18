46764d8bfa0d0ecb35936d5f5a186940990f57d5
Diego Lendoiro <diego@passbolt.com>
2024-12-18 16:31:28 +0000

    feature/PB 37679

--- tests/integration/tests/create_and_decrypt_test.sh
+++ tests/integration/tests/create_and_decrypt_test.sh
@@ -2,16 +2,17 @@
 
 function testCreateAndDecryptPassword {
   local value="password-example"
-  local test_id="$(date +'%s')"
+  local test_id=
+  test_id="$(date +'%s')"
   local username="email${test_id}@domain.tld"
-  _log Running "$test_id" test
-  registerPassboltUser $FIRSTNAME $LASTNAME "${username}" "$test_id"
+  _log Running "${test_id}" test
+  registerPassboltUser "${FIRSTNAME}" "${LASTNAME}" "${username}" "${test_id}"
   configurePassbolt "${username}"
-  password=$(createPassword "${test_id}" "${value}" "$test_id")
-  result=$("$PASSBOLT_CLI_BINARY" get resource --id $(echo $password | jq -r .id) -j | jq -r .password)
-  if [[ "$value" != "$result" ]]; then
-    >&2 echo "Expected \"$value\", got \"$result\""
+  password=$(createPassword "${test_id}" "${value}" "${test_id}")
+  result=$("${PASSBOLT_CLI_BINARY}" get resource --id "$(echo "${password}" | jq -r .id)" -j | jq -r .password)
+  if [[ "${value}" != "${result}" ]]; then
+    >&2 echo "Expected \"${value}\", got \"${result}\""
     return 1
   fi
-  _log Test "$test_id" ran succesfully
+  _log Test "${test_id}" ran succesfully
 }
--- tests/integration/tests/create_share_and_decrypt_password_test.sh
+++ tests/integration/tests/create_share_and_decrypt_password_test.sh
@@ -1,24 +1,27 @@
 #!/bin/bash
 
 function testCreateShareAndDecryptPassword {
-	local value="password-example"
-	local test_id="$(date +'%s')"
-	local source_username="source-${test_id}@domain.tld"
-	local destination_username="destination-${test_id}@domain.tld"
-	local logs
-	_log Running "$test_id" test
-	registerPassboltUser $FIRSTNAME $LASTNAME "${source_username}"
-	registerPassboltUser $FIRSTNAME $LASTNAME "${destination_username}"
-	local destination_user_id=$(getUserIdByUsername "${destination_username}")
-	configurePassbolt "${source_username}"
-	local id=$(createPassword "${test_id}" "${value}" "$test_id")
-	sharePassword "$(echo $id | jq -r .id)" "$destination_user_id" "15"
-	configurePassbolt "${destination_username}"
-	local result=$(getPasswordSecretById $(echo $id | jq -r .id))
-	if [[ "$value" != "$result" ]]; then
-		>&2 echo "Expected \"$value\", got \"$result\""
-		return 1
-	fi
-	_log Test "$test_id" ran succesfully
+  local value="password-example"
+  local test_id=
+  test_id="$(date +'%s')"
+  local source_username="source-${test_id}@domain.tld"
+  local destination_username="destination-${test_id}@domain.tld"
+  _log Running "${test_id}" test
+  registerPassboltUser "${FIRSTNAME}" "${LASTNAME}" "${source_username}"
+  registerPassboltUser "${FIRSTNAME}" "${LASTNAME}" "${destination_username}"
+  local destination_user_id=
+  destination_user_id=$(getUserIdByUsername "${destination_username}")
+  configurePassbolt "${source_username}"
+  local id=
+  id=$(createPassword "${test_id}" "${value}" "${test_id}")
+  sharePassword "$(echo "${id}" | jq -r .id)" "${destination_user_id}" "15"
+  configurePassbolt "${destination_username}"
+  local result=
+  result=$(getPasswordSecretById "$(echo "${id}" | jq -r .id)")
+  if [[ "${value}" != "${result}" ]]; then
+    >&2 echo "Expected \"${value}\", got \"${result}\""
+    return 1
+  fi
+  _log Test "${test_id}" ran succesfully
 
 }
